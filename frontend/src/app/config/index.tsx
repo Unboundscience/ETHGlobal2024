27fa660d09c897f70ecb9fa4c6914df0de8356dd
ighmaz <ighmazcool@gmail.com>
2024-09-21 01:30:42 +0800

    add config file

---
+++ frontend/src/app/config/index.tsx
@@ -0,0 +1,29 @@
+// config/index.tsx
+
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+
+import { cookieStorage, createStorage } from "wagmi";
+import { mainnet, sepolia } from "wagmi/chains";
+
+// Your Reown Cloud project ID
+export const projectId = "49ebd0d45eb047918ba810c68dd748d5";
+
+// Create a metadata object
+const metadata = {
+  name: "resume",
+  description: "AppKit Example",
+  url: "https://reown.com/appkit", // origin must match your domain & subdomain
+  icons: ["https://assets.reown.com/reown-profile-pic.png"],
+};
+
+// Create wagmiConfig
+const chains = [mainnet, sepolia] as const;
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+});
